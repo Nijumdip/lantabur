04a7bb33b84808cc5e959c5eeb029631e4a6f361
Nijumdip <nijumdip019911@gmail.com>
2022-11-03 17:49:29 +0600

    added navber

--- src/Pages/HomePage/Home.js
+++ src/Pages/HomePage/Home.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React from "react";
+import Navbar from "./Navbar";
 
 const Home = () => {
     return (
         <div>
-            This is home page
+            <Navbar /> 
+            
+            
         </div>
     );
 };
