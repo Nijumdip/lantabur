cbdd7136658be24ca5ab42827536a7cb227e2c27
Nijumdip <nijumdip019911@gmail.com>
2022-11-07 22:34:06 +0600

    edit some heroCards

--- src/Pages/HomePage/HeroCards.js
+++ src/Pages/HomePage/HeroCards.js
@@ -12,7 +12,65 @@ const HeroCards = () => {
                         <div class="flex items-center space-x-6 lg:space-x-8 ">
                             <div class="grid flex-shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
                                 
-                                <div class="h-64 w-44 overflow-hidden border rounded-lg sm:opacity-0 lg:opacity-100">
+                                <div class="h-64 w-44 overflow-hidden border border-green-900 rounded-lg sm:opacity-0 lg:opacity-100">
+                                    <div className="p-1 flex flex-col justify-center items-center ">
+                                        <img
+                                            src="https://tailwindui.com/img/ecommerce-images/home-page-03-hero-image-tile-01.jpg"
+                                            alt=""
+                                            class="h-20 w-20 rounded-full object-cover "
+                                        />
+
+                                        <h4 className="text-xl font-semibold tracking-tight text-green-600">
+                                            Lantabur IT
+                                        </h4>
+                                        <p className="mb-2 leading-normal">
+                                            Provided web development, app development, any kind of
+                                            software development service
+                                        </p>
+                                        <div className="p-4"></div>
+                                    </div>
+                                </div>
+
+                                <div class="h-64 w-44 overflow-hidden border border-green-900 rounded-lg sm:opacity-0 lg:opacity-100">
+                                    <div className="p-1 flex flex-col justify-center items-center ">
+                                        <img
+                                            src="https://tailwindui.com/img/ecommerce-images/home-page-03-hero-image-tile-01.jpg"
+                                            alt=""
+                                            class="h-20 w-20 rounded-full object-cover "
+                                        />
+
+                                        <h4 className="text-xl font-semibold tracking-tight text-green-600">
+                                            Programming Pathshala
+                                        </h4>
+                                        <p className="mb-2 leading-normal">
+                                            This will be a programming learning platform
+                                        </p>
+                                        <div className="p-4"></div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="grid flex-shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
+                                
+                                <div class="h-64 w-44 overflow-hidden border border-green-900 rounded-lg sm:opacity-0 lg:opacity-100">
+                                    <div className="p-1 flex flex-col justify-center items-center ">
+                                        <img
+                                            src="https://tailwindui.com/img/ecommerce-images/home-page-03-hero-image-tile-01.jpg"
+                                            alt=""
+                                            class="h-20 w-20 rounded-full object-cover "
+                                        />
+
+                                        <h4 className="text-xl font-semibold tracking-tight text-green-600">
+                                            Programming Pathshala
+                                        </h4>
+                                        <p className="mb-2 leading-normal">
+                                            This will be a programming learning platform
+                                        </p>
+                                        <div className="p-4"></div>
+                                    </div>
+                                </div>
+
+                                <div class="h-64 w-44 overflow-hidden border border-green-900 rounded-lg sm:opacity-0 lg:opacity-100">
                                     
                                     <div className="p-1 flex flex-col justify-center items-center ">
                                         <img
@@ -21,19 +79,18 @@ const HeroCards = () => {
                                             class="h-20 w-20 rounded-full object-cover "
                                         />
 
-                                        <h4 className="text-xl font-semibold tracking-tight text-blue-600">
-                                        Lantabur IT
+                                        <h4 className="text-xl font-semibold tracking-tight text-green-600">
+                                        Programming Pathshala
                                         </h4>
                                         <p className="mb-2 leading-normal">
-                                        Provided web development, app development, any kind of software development service
-
+                                        This will be a programming learning platform
                                         </p>
                                         <div className="p-4">
                                         </div>
                                     </div>
                                 </div>
 
-                                <div class="h-64 w-44 overflow-hidden border rounded-lg sm:opacity-0 lg:opacity-100">
+                                <div class="h-64 w-44 overflow-hidden border border-green-900 rounded-lg sm:opacity-0 lg:opacity-100">
                                     
                                     <div className="p-1 flex flex-col justify-center items-center ">
                                         <img
@@ -42,7 +99,7 @@ const HeroCards = () => {
                                             class="h-20 w-20 rounded-full object-cover "
                                         />
 
-                                        <h4 className="text-xl font-semibold tracking-tight text-blue-600">
+                                        <h4 className="text-xl font-semibold tracking-tight text-green-600">
                                         Programming Pathshala
                                         </h4>
                                         <p className="mb-2 leading-normal">
@@ -55,43 +112,45 @@ const HeroCards = () => {
                             </div>
 
                             <div class="grid flex-shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
-                                <div class="h-64 w-44 overflow-hidden rounded-lg">
+                                
+                                <div class="h-64 w-44 overflow-hidden border border-green-900 rounded-lg sm:opacity-0 lg:opacity-100">
+                                        
+                                <div className="p-1 flex flex-col justify-center items-center ">
                                     <img
-                                        src="https://tailwindui.com/img/ecommerce-images/home-page-03-hero-image-tile-03.jpg"
+                                        src="https://tailwindui.com/img/ecommerce-images/home-page-03-hero-image-tile-01.jpg"
                                         alt=""
-                                        class="h-full w-full object-cover object-center"
+                                        class="h-20 w-20 rounded-full object-cover "
                                     />
-                                </div>
-                                <div class="h-64 w-44 overflow-hidden rounded-lg">
-                                    <img
-                                        src="https://tailwindui.com/img/ecommerce-images/home-page-03-hero-image-tile-04.jpg"
-                                        alt=""
-                                        class="h-full w-full object-cover object-center"
-                                    />
-                                </div>
-                                <div class="h-64 w-44 overflow-hidden rounded-lg">
-                                    <img
-                                        src="https://tailwindui.com/img/ecommerce-images/home-page-03-hero-image-tile-05.jpg"
-                                        alt=""
-                                        class="h-full w-full object-cover object-center"
-                                    />
-                                </div>
-                            </div>
 
-                            <div class="grid flex-shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
-                                <div class="h-64 w-44 overflow-hidden rounded-lg">
-                                    <img
-                                        src="https://tailwindui.com/img/ecommerce-images/home-page-03-hero-image-tile-06.jpg"
-                                        alt=""
-                                        class="h-full w-full object-cover object-center"
-                                    />
+                                    <h4 className="text-xl font-semibold tracking-tight text-green-600">
+                                    Programming Pathshala
+                                    </h4>
+                                    <p className="mb-2 leading-normal">
+                                    This will be a programming learning platform
+                                    </p>
+                                    <div className="p-4">
+                                    </div>
                                 </div>
-                                <div class="h-64 w-44 overflow-hidden rounded-lg">
-                                    <img
-                                        src="https://tailwindui.com/img/ecommerce-images/home-page-03-hero-image-tile-07.jpg"
-                                        alt=""
-                                        class="h-full w-full object-cover object-center"
-                                    />
+                                </div>
+
+                                <div class="h-64 w-44 overflow-hidden border border-green-900 rounded-lg sm:opacity-0 lg:opacity-100">
+                                    
+                                    <div className="p-1 flex flex-col justify-center items-center ">
+                                        <img
+                                            src="https://tailwindui.com/img/ecommerce-images/home-page-03-hero-image-tile-01.jpg"
+                                            alt=""
+                                            class="h-20 w-20 rounded-full object-cover "
+                                        />
+
+                                        <h4 className="text-xl font-semibold tracking-tight text-green-600">
+                                        Programming Pathshala
+                                        </h4>
+                                        <p className="mb-2 leading-normal">
+                                        This will be a programming learning platform
+                                        </p>
+                                        <div className="p-4">
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
