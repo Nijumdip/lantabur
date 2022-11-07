24f17bf6fe81bdf24d9016f1d12437b6b35e2fac
Nijumdip <nijumdip019911@gmail.com>
2022-11-07 22:15:06 +0600

    edit some heroCards

--- src/Pages/HomePage/HeroCards.js
+++ src/Pages/HomePage/HeroCards.js
@@ -11,33 +11,46 @@ const HeroCards = () => {
                     <div class="absolute transform sm:left-1/2 sm:top-0 sm:translate-x-8 lg:left-1/2 lg:top-1/2 lg:-translate-y-1/2 lg:translate-x-8">
                         <div class="flex items-center space-x-6 lg:space-x-8 ">
                             <div class="grid flex-shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
+                                
                                 <div class="h-64 w-44 overflow-hidden border rounded-lg sm:opacity-0 lg:opacity-100">
-                                    <div>
+                                    
+                                    <div className="p-1 flex flex-col justify-center items-center ">
                                         <img
                                             src="https://tailwindui.com/img/ecommerce-images/home-page-03-hero-image-tile-01.jpg"
                                             alt=""
-                                            class="h-10 w-10 object-cover object-center"
+                                            class="h-20 w-20 rounded-full object-cover "
                                         />
 
+                                        <h4 className="text-xl font-semibold tracking-tight text-blue-600">
+                                        Lantabur IT
+                                        </h4>
+                                        <p className="mb-2 leading-normal">
+                                        Provided web development, app development, any kind of software development service
+
+                                        </p>
                                         <div className="p-4">
-                                            <h4 className="text-xl font-semibold tracking-tight text-blue-600">
-                                                Web Development
-                                            </h4>
-                                            <p className="mb-2 leading-normal">
-                                                Holisticly morph distinctive methodologies and
-                                                vertical e-tailers. Rapidiously grow customized
-                                                data.
-                                            </p>
                                         </div>
                                     </div>
                                 </div>
 
-                                <div class="h-64 w-44 overflow-hidden rounded-lg">
-                                    <img
-                                        src="https://tailwindui.com/img/ecommerce-images/home-page-03-hero-image-tile-02.jpg"
-                                        alt=""
-                                        class="h-full w-full object-cover object-center"
-                                    />
+                                <div class="h-64 w-44 overflow-hidden border rounded-lg sm:opacity-0 lg:opacity-100">
+                                    
+                                    <div className="p-1 flex flex-col justify-center items-center ">
+                                        <img
+                                            src="https://tailwindui.com/img/ecommerce-images/home-page-03-hero-image-tile-01.jpg"
+                                            alt=""
+                                            class="h-20 w-20 rounded-full object-cover "
+                                        />
+
+                                        <h4 className="text-xl font-semibold tracking-tight text-blue-600">
+                                        Programming Pathshala
+                                        </h4>
+                                        <p className="mb-2 leading-normal">
+                                        This will be a programming learning platform
+                                        </p>
+                                        <div className="p-4">
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
 
